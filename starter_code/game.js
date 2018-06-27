a4b68f699eb51e3cf3f8df07c352fa0376e950c4
Pablo Diaz <pdiaz.ieu2014@student.ie.edu>
2018-06-27 20:02:28 +0200

    Changed to original state with 2 obstacles

--- starter_code/game.js
+++ starter_code/game.js
@@ -14,53 +14,58 @@ Game.prototype.reset = function() {
   this.score.points = 0;
   this.obstacles = [];
   this.enemy = [];
-  
-  console.log(this.enemy) ;
+  // console.log(this.enemy) ; 
   this.generateEnemy();
-  
+
   this.framesCounter = 0;
   this.framesTime = 0;
-  console.log(framesTime);
   this.obstacleNumber = 6;
   this.generateObstacle();
-  this.score.lives = 10;
+  this.score.lives = 10
 };
 
 Game.prototype.start = function() {
   this.interval = setInterval(
     function() {
+ 
       this.framesCounter++;
       this.framesTime++;
       if (this.framesCounter > 1000) this.framesCounter = 0;
       // console.log(this.framesTime);
 
       this.clear();
-      this.draw();
-      //this.background.draw();
+      this.background.draw();
       this.background.move();
-      //this.player.draw();
+      this.player.draw();
       this.player.move();
+      this.enemy[0].draw();
+      this.enemy[0].move();
       this.updateObs();
-      this.enemyUpdate();
-      //this.enemyCollision();
-      //console.log(this.framesTime);
+      console.log(this.enemy)
 
-      if (this.framesTime < 400) {
-        if (this.framesCounter % 200 === 0) {
-          this.generateEnemy(1);
+      
+      if (this.framesTime < 900) {
+         
+        if (this.framesCounter % 150 === 0) {
+          console.log("sdsdsdsd")
+          this.enemy.pop();
+          this.generateEnemy();
         }
-      } else {
-        this.maxLife = true;
+      } else if (this.framesTime > 1000 ) {
+          this.maxLife = true;
+        
 
-        if (this.framesCounter % 200 === 0) {
-          this.generateEnemy(2);
+      
+        if (this.framesCounter % 40 === 0) {
+          this.generateEnemy();
         }
-        if (this.framesCounter % 400 === 0) {
-          this.generateEnemy(4);
+        if (this.framesCounter % 1000 === 0) {
+          
         }
       }
-
-      if (this.score.health <= 0) {
+      
+      if (this.score.health == 0) {
+        
         this.gameOver();
       }
     }.bind(this),
@@ -68,19 +73,9 @@ Game.prototype.start = function() {
   );
 };
 
-//Draw//
-
-Game.prototype.draw = function () {
-  this.background.draw ();
-  this.player.draw ();
-  this.score.drawScore ();
-  this.score.drawHealth ();
-}
-
-
-//Game Over
 Game.prototype.gameOver = function() {
   this.stop();
+
 };
 
 Game.prototype.stop = function() {
@@ -93,84 +88,67 @@ Game.prototype.generateObstacle = function() {
   //for loop
   for (var i = 0; i < this.obstacleNumber; i++) {
     this.obstacles.push(new Obstacle(this));
+  } 
+}  
+Game.prototype.generateEnemy = function() {
+  this.enemy.push(new ObstacleEnemy(this));
+}
+
+Game.prototype.g
     
-  }
-};
-Game.prototype.generateEnemy = function(v) {
-  for (i = 0; i < v; i++) {
-    this.enemy.push(new ObstacleEnemy(this));
-  }
-};
+ Game.prototype.updateObs = function() {
 
-Game.prototype.enemyUpdate = function() {
-  for (i = 0; i < this.enemy.length; i++) {
-    this.enemy[i].draw();
-    this.enemy[i].move();
-
-     if (collision) {
-       this.enemy[i].y= 0;
-       this.enemy[i].x= 
-       this.score.health -= 5;
-
-     }
-
-  }
-};
-
-Game.prototype.updateObs = function() {
   for (var i = 0; i < this.obstacleNumber; i++) {
+    
+  
     this.obstacles[i].move();
     this.obstacles[i].draw();
+   
 
     if (this.obstacles[i].y >= 750) {
       this.obstacles[i].y = 0;
       this.obstacles[i].x = Math.random() * 600;
-      
     }
+    if(this.player.x < this.obstacles[i].x + this.obstacles[i].w &&
+      this.player.x + this.player.w > this.obstacles[i].x &&
+      this.player.y < this.obstacles[i].y + this.obstacles[i].h &&
+      this.player.h + this.player.y > this.obstacles[i].y) {
+        this.obstacles[i].y=0
+        this.obstacles[i].x = Math.random()*(canvas.width);
+        this.obstacles.push(new Obstacle(this));
+        this.obstacleNumber++
+        this.score.health -=5
+        // console.log(this.score.health)
+        if (this.obstacleNumber > 20){
+          this.obstacleNumber=20
+          this.obstacles.pop()
+          console.log(this.obstacles)
+        }
+      
+        //console.log(this.obstacles)
 
-    var collision = 
-        this.player.x < this.obstacles[i].x + this.obstacles[i].w &&
-        this.player.x + this.player.w > this.obstacles[i].x &&
-        this.player.y < this.obstacles[i].y + this.obstacles[i].h &&
-        this.player.h + this.player.y > this.obstacles[i].y
-
-    if (collision) {
-      
-      this.obstacles[i].y = 0;
-      this.obstacles[i].x = Math.random() * canvas.width;
-      this.obstacles.push(new Obstacle(this));
-      this.obstacleNumber++;
-      this.score.health -= 5;
-      
-      
-      // console.log(this.score.health)
-      if (this.obstacleNumber > 20) {
-        this.obstacleNumber = 20;
-        this.obstacles.pop();
-        //console.log(this.obstacles);
       }
-
-      //console.log(this.obstacles)
     }
   }
+
+
+Game.prototype.isCollision = function() {
+  return this.obstacles.some(
+    function(obstacle) {
+      return (
+        this.player.x < obstacle.x + obstacle.w &&
+        this.player.x + this.player.w > obstacle.x &&
+        this.player.y < obstacle.y + obstacle.h &&
+        this.player.h + this.player.y > obstacle.y
+        
+      ); {
+        
+        
+      }
+
+    }.bind(this)
+  );
 };
-
-
-
-// Game.prototype.enemyCollision= function () {
-// if (collision) {
-  
-// }
-// }
-// Game.prototype.isCollision = function() {
-//   return this.obstacles.some(
-//     function(obstacle) {
-//       return collision;
-//       {
-//       }
-//     }.bind(this)
-//   );
-// };
 
 Game.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
