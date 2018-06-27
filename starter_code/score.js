0b37bb18db399053553d0bf5ef1ea47aa3329ba1
Pablo Diaz <pdiaz.ieu2014@student.ie.edu>
2018-06-27 19:59:17 +0200

    Errors in new obstacle

--- starter_code/score.js
+++ starter_code/score.js
@@ -1,21 +1,21 @@
 function Score(game){
   this.game = game;
 
-  this.points = 0;
+  this.points = this.game.framesCounter;
   this.health = 100;
 
 }
  
 Score.prototype.drawScore = function(){
-  this.game.ctx.font = "";
+  this.game.ctx.font = "Arial";
   this.game.ctx.fillStyle = '#fff';
-  this.game.ctx.fillText('Score:'+Math.floor(this.points), 630.5, 25);
+  this.game.ctx.fillText('Score:'+(this.points), 500, 600);
   this.game.ctx.textBaseline = "top";
 };
 
-Score.prototype.drawLives = function(){
-  this.game.ctx.font = "14px 'Press Start 2P'";
+Score.prototype.drawHealth = function(){
+  this.game.ctx.font = "14px Arial";
   this.game.ctx.fillStyle = '#fff';
-  this.game.ctx.fillText('Lives:'+Math.floor(this.lives), 1150, 25);
+  this.game.ctx.fillText('Health:'+(this.health), 300, 300);
   this.game.ctx.textBaseline = "top";
 };
